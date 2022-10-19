c42da6b9e3c6dc20b0fdb6ae9e7bc74bbbd37f67
dengkai.chang <dengkai.chang@idreamsky.com>
2022-10-19 19:27:44 +0800

    feat: 实现读取excel并查询poi地址输出为excel

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ import pdfplumber
 import re
 import numpy as np
 import pandas as pd
+import requests
+import json
 
-
-# 97,山南地区
-# 98,林芝地区
-# 99,昌都地区
-# 100,拉萨市
-# 101,那曲地区
-# 102,日喀则地区
-# 103,阿里地区
 
 def parse_people(path):
     pdf = pdfplumber.open(path)
@@ -52,9 +46,44 @@ def find_all_file():
                 parse_location(file)
 
 
+def search_address(row):
+    return request_point_from_address(row["city"], row["address"])
+
+
 def read_location_from_excel(path):
-    df = pd.read_excel(path)
-    print(df.head())
+    df_red = pd.read_excel(path, sheet_name="red")
+    df_red[["poi", "lat", "lng"]] = df_red.apply(search_address, axis=1, result_type="expand")
+    df_red.to_excel("asset/林芝市测试数据_修改后.xlsx", sheet_name="red")
+
+    df_yellow = pd.read_excel(path, sheet_name="yellow")
+    df_yellow[["poi", "lat", "lng"]] = df_yellow.apply(search_address, axis=1, result_type="expand")
+    df_yellow.to_excel("asset/林芝市测试数据_修改后.xlsx", sheet_name="yellow")
+
+    writer = pd.ExcelWriter("asset/林芝市测试数据_修改后.xlsx")
+    df_red.to_excel(writer, sheet_name="red")
+    df_yellow.to_excel(writer, sheet_name="yellow")
+    writer.close()
+
+
+def request_point_from_address(city, address):
+    print("开始查询:" + city + address)
+    codeMap = {"山南市": "97", "林芝市": "98", "昌都市": "99", "拉萨市": "100", "那曲市": "101", "日喀则市": "100", "阿里地区": "103"}
+    url = "https://api.map.baidu.com/place/v2/search"
+    param = {"output": "json", "scope": "2", "ak": "EBRZYvda30n9EdMvL3k4veu8i7EeCsac", "region": city, "city_limit": codeMap[city], "query": address}
+
+    response = requests.get(url, params=param)
+    if response.status_code == 200:
+        jsonResponse = json.loads(response.text)
+        if not ((jsonResponse is None) or (jsonResponse["results"] is None) or (len(jsonResponse["results"]) <= 0)):
+            firstResult = jsonResponse["results"][0]
+            if not firstResult.get("location") is None:
+                # print("查询 " + address + " 成功：" + str(firstResult))
+                return firstResult["name"], firstResult["location"]["lat"], firstResult["location"]["lng"]
+            else:
+                print("查询 " + address + " ---- 未返回location ：" + str(firstResult))
+    else:
+        print("请求http返回值异常：" + response.status_code)
+    return "地址解析异常", 0, 0
 
 
 if __name__ == '__main__':
@@ -62,6 +91,7 @@ if __name__ == '__main__':
     # parse_location("asset/1.pdf")
     # find_all_file()
     read_location_from_excel("asset/林芝市测试数据.xlsx")
+    # print(request_point_from_address("日喀则市", "拉孜县曲下镇上退休基地东侧第三排"))
 
 # 林芝
 # GET https://api.map.baidu.com/place/v2/search?query=巴宜区百盛药业有限公司&region=林芝地区&city_limit=98&output=json&scope=2&ak=EBRZYvda30n9EdMvL3k4veu8i7EeCsac
